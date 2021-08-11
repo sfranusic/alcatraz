096c1d53f9f8d4638f7d25a5b34925a180956b7c
Samuel Franusic <sfranusic@gmail.com>
2021-08-11 16:35:02 -0700

    Add Xcode project

---
+++ Mesmerize/Shared/MesmerizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MesmerizeApp.swift
+//  Shared
+//
+//  Created by Sammy Franusic on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct MesmerizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

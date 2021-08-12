cf05c465fc3db51f2089b74df7949048b8ed6a19
Samuel Franusic <sfranusic@gmail.com>
2021-08-12 15:29:32 -0700

    Add tvOS target

---
+++ Mesmerize/Mesmerize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mesmerize
+//
+//  Created by Sammy Franusic on 8/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mesmerize/Mesmerize/MesmerizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MesmerizeApp.swift
+//  Mesmerize
+//
+//  Created by Sammy Franusic on 8/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MesmerizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

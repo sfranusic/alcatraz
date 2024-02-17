a33f35e0ff1a645cdcaf9f812f3de2c1e37b4fdd
Sam Franusic <sfranusic@gmail.com>
2024-02-16 20:41:35 -0800

    Add new project

---
+++ Lab/Lab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab
+//
+//  Created by Sam Franusic on 2/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lab/Lab/LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabApp.swift
+//  Lab
+//
+//  Created by Sam Franusic on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

7c68695ebd073e7acc234d47a12eb16ba95ca205
Sam Franusic <sfranusic@gmail.com>
2024-02-16 22:34:01 -0800

    Update to app storage property wrapper

--- Lab/Lab/EmojigmaV2/EmojigmaPickerModel.swift
+++ Lab/Lab/EmojigmaV2/EmojigmaPickerModel.swift
@@ -8,24 +8,16 @@
 import SwiftUI
 
 class EmojigmaPickerModel: ObservableObject {
-    @Published var rotorSelections: [String]
+    @Published var rotorSelections = [String]()
+    @AppStorage("rotor1") var rotor1: String = ""
+    @AppStorage("rotor2") var rotor2: String = ""
+    @AppStorage("rotor3") var rotor3: String = ""
 
     init() {
-        rotorSelections = [
-            UserDefaults.standard.string(forKey: "firstRotorSelection") ?? emojiSet.first!,
-            UserDefaults.standard.string(forKey: "secondRotorSelection") ?? emojiSet.first!,
-            UserDefaults.standard.string(forKey: "thirdRotorSelection") ?? emojiSet.first!,
-        ]
-    }
-
-    deinit {
-        saveSelectionState()
-    }
-
-    func saveSelectionState() {
-        UserDefaults.standard.setValue(rotorSelections[0], forKey: "firstRotorSelection")
-        UserDefaults.standard.setValue(rotorSelections[1], forKey: "secondRotorSelection")
-        UserDefaults.standard.setValue(rotorSelections[2], forKey: "thirdRotorSelection")
+        rotor1 = emojiSet.first ?? ""
+        rotor2 = emojiSet.first ?? ""
+        rotor3 = emojiSet.first ?? ""
+        rotorSelections = [rotor1, rotor2, rotor3]
     }
 
     let emojiSet: [String] = [

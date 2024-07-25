ff2cd95f0e4edd2a9046b35919f492fc4d4fd5b0
Sam Franusic <sfranusic@gmail.com>
2024-07-25 14:03:45 -0700

    Fix bug for wrong button mapping

---
+++ lab/lab/Controller/ControllerModel.swift
@@ -0,0 +1,82 @@
+//
+//  ControllerModel.swift
+//  lab
+//
+//  Created by Sam Franusic on 7/23/24.
+//
+
+import GameController
+
+class ControllerModel: ObservableObject {
+    @Published var connectedToController: Bool = false
+    @Published var circleButtonPressed: Bool = false
+    @Published var exButtonPressed: Bool = false
+    @Published var leftThumbstickX: Float = 0.0
+    @Published var leftThumbstickY: Float = 0.0
+    @Published var rightThumbstickX: Float = 0.0
+    @Published var rightThumbstickY: Float = 0.0
+
+    var controller: GCController?
+
+    init() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(controllerConnected),
+            name: .GCControllerDidConnect,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(controllerDisconnected),
+            name: .GCControllerDidDisconnect,
+            object: nil
+        )
+
+        if let connectedController = GCController.controllers().first {
+            self.controller = connectedController
+            setupController(controller: connectedController)
+        }
+
+        GCController.startWirelessControllerDiscovery(completionHandler: nil)
+    }
+
+    @objc func controllerConnected(notification: Notification) {
+        if let controller = notification.object as? GCController {
+            self.controller = controller
+            setupController(controller: controller)
+            connectedToController = true
+        }
+    }
+
+    @objc func controllerDisconnected(notification: Notification) {
+        if let controller = notification.object as? GCController, controller == self.controller {
+            self.controller = nil
+            connectedToController = false
+        }
+    }
+
+    func setupController(controller: GCController) {
+        guard let gamepad = controller.extendedGamepad else {
+            print("Extended gamepad not found")
+            return
+        }
+
+        gamepad.buttonA.pressedChangedHandler = { [weak self] (button, value, pressed) in
+            self?.exButtonPressed = pressed
+        }
+
+        gamepad.buttonB.pressedChangedHandler = { [weak self] (button, value, pressed) in
+            self?.circleButtonPressed = pressed
+        }
+
+        gamepad.leftThumbstick.valueChangedHandler = { [weak self] (_, xValue, yValue) in
+            self?.leftThumbstickX = xValue
+            self?.leftThumbstickY = yValue
+        }
+
+        gamepad.rightThumbstick.valueChangedHandler = { [weak self] (dpad, xValue, yValue) in
+            self?.rightThumbstickX = xValue
+            self?.rightThumbstickY = yValue
+        }
+    }
+}
--- lab/lab/Controller/MockControllerView.swift
+++ lab/lab/Controller/MockControllerView.swift
@@ -10,13 +10,27 @@ import SwiftUI
 struct MockControllerView: View {
     @StateObject private var viewModel = ControllerModel()
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
+            controllerConnectionView
             Text("O Button: \(viewModel.circleButtonPressed ? "Pressed" : "Not Pressed")")
             Text("X Button: \(viewModel.exButtonPressed ? "Pressed" : "Not Pressed")")
             Text("Left Thumbstick: (\(viewModel.leftThumbstickX), \(viewModel.leftThumbstickY))")
             Text("Right Thumbstick: (\(viewModel.rightThumbstickX), \(viewModel.rightThumbstickY))")
         }
         .padding()
+    }
+
+    private var controllerConnectionView: some View {
+        HStack {
+            Image(systemName: "gamecontroller")
+            if viewModel.connectedToController {
+                Image(systemName: "checkmark")
+                    .foregroundColor(.green)
+            } else {
+                Image(systemName: "exclamationmark.triangle")
+                    .foregroundColor(.red)
+            }
+        }
     }
 }
 

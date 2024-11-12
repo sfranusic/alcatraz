f4376e3efc2fc20e82cf793195797aa4a4e6d58c
Sam Franusic <sfranusic@gmail.com>
2024-11-11 21:10:23 -0800

    Add new ios project

---
+++ ios/Murcielago/Murcielago/MurcielagoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MurcielagoApp.swift
+//  Murcielago
+//
+//  Created by Sam Franusic on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MurcielagoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

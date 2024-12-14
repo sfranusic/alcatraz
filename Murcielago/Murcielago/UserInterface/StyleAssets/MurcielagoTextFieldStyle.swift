136418ac6abddb70c8b9ffcd80c65e4203dcc2ce
Sam Franusic <sfranusic@gmail.com>
2024-12-13 22:37:45 -0800

    Create text field style and adjust hittable area for fields

---
+++ Murcielago/Murcielago/UserInterface/StyleAssets/MurcielagoTextFieldStyle.swift
@@ -0,0 +1,47 @@
+//
+//  MurcielagoTextFieldStyle.swift
+//  Murcielago
+//
+//  Created by Sam Franusic on 12/13/24.
+//
+
+import SwiftUI
+
+struct MurcielagoTextFieldStyle: TextFieldStyle {
+    @FocusState private var textFieldFocused: Bool
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding(.leading, 15)
+            .frame(height: 50)
+            .background {
+                RoundedRectangle(cornerRadius: 8.0)
+                    .fill(Color.surface)
+            }
+            .overlay {
+                RoundedRectangle(cornerRadius: 8.0)
+                    .stroke(lineWidth: 1.0)
+            }
+            .focused($textFieldFocused)
+            .onTapGesture {
+                textFieldFocused = true
+            }
+    }
+}
+
+extension TextFieldStyle where Self == MurcielagoTextFieldStyle {
+    static var murcielago: Self {
+        .init()
+    }
+}
+
+#Preview {
+    @Previewable @State var passwordInput: String = ""
+    @Previewable @State var usernameInput: String = ""
+    VStack {
+        TextField("Username", text: $usernameInput)
+            .textFieldStyle(.murcielago)
+        SecureField("Password", text: $passwordInput)
+            .textFieldStyle(.murcielago)
+    }
+    .preferredColorScheme(.dark)
+}

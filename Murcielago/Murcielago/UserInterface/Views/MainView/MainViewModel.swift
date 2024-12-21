4371373d0fa13b486ac9da5e7b641e0c80baf0cc
Sam Franusic <sfranusic@gmail.com>
2024-12-20 21:47:52 -0800

    Create method to announce error messages

--- Murcielago/Murcielago/UserInterface/Views/MainView/MainViewModel.swift
+++ Murcielago/Murcielago/UserInterface/Views/MainView/MainViewModel.swift
@@ -60,6 +60,15 @@ class MainViewModel: ObservableObject {
         }
     }
 
+    /// This method uses `UIAccessibility` to announce the error message.
+    private func announceErrorMessage() {
+        let message = errorMessage
+        Task.detached {
+            try? await Task.sleep(for: .seconds(0.1))
+            await UIAccessibility.post(notification: .announcement, argument: message)
+        }
+    }
+
     @MainActor
     private func clearErrorMessage() {
         errorMessage = ""

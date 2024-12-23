a77e6561c7ffd316afd00ad254b41760599e1e33
Sam Franusic <sfranusic@gmail.com>
2024-12-22 21:11:24 -0800

    Disable UI tests for now

--- Murcielago/MurcielagoTests/MainViewModelTests.swift
+++ Murcielago/MurcielagoTests/MainViewModelTests.swift
@@ -10,8 +10,41 @@ import Testing
 struct MainViewModelTests {
 
     @Test func testInitialState() async throws {
-        let testViewModel = MainViewModel(authenticationService: .mock)
-        #expect(testViewModel.errorMessage == "")
+        let testViewModel = await MainViewModel(authenticationService: .mock)
+        #expect(await testViewModel.errorMessage == "")
+    }
+
+    @Test func testInitialTextFieldStates() async throws {
+        let testViewModel = await MainViewModel(authenticationService: .mock)
+        #expect(await testViewModel.passwordInput == "")
+        #expect(await testViewModel.usernameInput == "")
+    }
+
+    @Test func testSignInErrorWithNoCredentials() async throws {
+        let testViewModel = await MainViewModel(authenticationService: .mock)
+        await _ = testViewModel.signIn()
+        #expect(await testViewModel.errorMessage == SignInError.noCredentials.localizedDescription)
+    }
+
+    @Test func testSignInErrorWithBadCredentials() async throws {
+        let testViewModel = await MainViewModel(authenticationService: .mock)
+        await MainActor.run {
+            testViewModel.usernameInput = "badUsername"
+            testViewModel.passwordInput = "badPassword"
+        }
+        await _ = testViewModel.signIn()
+        #expect(await testViewModel.errorMessage == SignInError.invalidCredentials.localizedDescription)
+    }
+
+    @Test func testSignInSuccess() async throws {
+        let testViewModel = await MainViewModel(authenticationService: .mock)
+        await MainActor.run {
+            testViewModel.usernameInput = "username"
+            testViewModel.passwordInput = "password124"
+        }
+        let result = await testViewModel.signIn()
+        #expect(result)
+        #expect(await testViewModel.unauthenticated == false)
     }
 
 }

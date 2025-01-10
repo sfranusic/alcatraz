e5cdce9ff0ef5d59feedcd1e0f1c8ffa69639085
Sam Franusic <sfranusic@gmail.com>
2025-01-09 20:40:06 -0800

    Updates to project file

--- Murcielago/Murcielago/UserInterface/Views/GameView/GameSceneView.swift
+++ Murcielago/Murcielago/UserInterface/Views/GameView/GameSceneView.swift
@@ -5,14 +5,37 @@
 //  Created by Sam Franusic on 12/9/24.
 //
 
+import RealityKit
 import SwiftUI
 
-struct GameSceneView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    GameSceneView()
-}
+//struct GameSceneView: UIViewRepresentable {
+//    let sceneName: String
+//
+//    func makeUIView(context: Context) -> ARView{
+//
+//        guard let modelEntity = try? ModelEntity.load(named: sceneName) else {
+//            fatalError("Failed to load scene")
+//        }
+//        let anchorEntity = AnchorEntity()
+//        anchorEntity.addChild(modelEntity)
+//        let arView = ARView(frame: .zero)
+//        arView.scene.anchors.append(anchorEntity)
+//
+//        return arView
+//    }
+//
+//    func updateUIView(_ uiView: ARView, context: Context) {
+//
+//    }
+//}
+//
+//#Preview {
+//    ZStack {
+//        Color.primary
+//            .ignoresSafeArea()
+//        VStack {
+//            Text("Start Game")
+//            GameSceneView(sceneName: "foo")
+//        }
+//    }
+//}
